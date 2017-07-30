d24e2f356796250a9d28accd97a733cb88ed77bb
lishuai17 <lishuai17@xdf.cn>
2017-07-30 18:55:52 +0800

    pom修改

--- src/main/java/com/xdf/vps/dao/base/BaseDao.java
+++ src/main/java/com/xdf/vps/dao/base/BaseDao.java
@@ -1,5 +1,7 @@
 package com.xdf.vps.dao.base;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.InsertProvider;
 import org.apache.ibatis.annotations.SelectProvider;
 
@@ -9,6 +11,16 @@ public interface BaseDao {
 	
 	@InsertProvider(type=DyncSqlProvider.class,method="save")
 	public <T> void save(T po);
+	
+	public <T> void saveBatch(List<T> pos);
+	
+	public <T> void update(T po);
+	
+	public <T> void deleteById(T po);
+	
+	public <T> List<T> findAll(Class<T> clazz);
+	
+	public <T> List<T> find(Class<T> clazz);
 	
 	@SelectProvider(type=DyncSqlProvider.class,method="getById")
 	public <T,ID> T getById(ID id);
